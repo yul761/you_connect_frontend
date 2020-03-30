15d3bbafb550474c643da37e2a27d47b2cd26d0c
Yuchen Lin <kloselyc@gmail.com>
2020-03-29 18:20:39 -0600

    initialize frontend and test connection with backend on aws

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+var backendUrl = "http://3.15.233.84:4000";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      curUser: undefined,
+      curToken: undefined
+    };
+  }
+  componentDidMount() {}
+
+  signupForm = e => {
+    e.preventDefault();
+    console.log(e.target.username.value);
+    let username = e.target.username.value;
+    let email = e.target.email.value;
+    let password = e.target.password.value;
+    let profile = {
+      username: username,
+      email: email,
+      password: password
+    };
+
+    Axios.post(`${backendUrl}/auth/signup`, profile).then(response => {
+      console.log(response);
+    });
+  };
+
+  loginForm = e => {
+    e.preventDefault();
+    let email = e.target.email.value;
+    let password = e.target.password.value;
+    let profile = {
+      email: email,
+      password: password
+    };
+
+    Axios.post(`${backendUrl}/auth/login`, profile).then(response => {
+      console.log(response);
+      this.setState({ curToken: response.data.accesstoken });
+    });
+  };
+
+  userprofile = () => {
+    console.log(this.state.curToken);
+    Axios.get(`${backendUrl}/userInfo`, {
+      headers: { "auth-token": this.state.curToken }
+    }).then(response => {
+      // this.setState({ curUser: response.data });
+      console.log(response.data);
+    });
+  };
+
+  postOne = () => {
+    let profile = {
+      content: "frontend test",
+      images: "images frontend test",
+      videos: "video frontend test",
+      likes: 2
+    };
+    Axios.post(`${backendUrl}/post/add`, profile, {
+      headers: { "auth-token": this.state.curToken }
+    }).then(response => {
+      // this.setState({ curUser: response.data });
+      console.log(response.data);
+    });
+  };
+  render() {
+    return (
+      <div>
+        <form
+          onSubmit={e => {
+            this.signupForm(e);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <label>
+            UserName :<input type="text" name="username"></input>
+          </label>
+          <label>
+            Email :<input type="email" name="email"></input>
+          </label>
+          <label>
+            password :<input type="password" name="password"></input>
+          </label>
+
+          <input type="submit" value="Submit" />
+        </form>
+
+        <form
+          onSubmit={e => {
+            this.loginForm(e);
+          }}
+        >
+          <label>
+            Email :<input type="email" name="email"></input>
+          </label>
+          <label>
+            password :<input type="password" name="password"></input>
+          </label>
+
+          <input type="submit" value="Submit" />
+        </form>
+
+        <button onClick={() => this.userprofile()}>check user profile</button>
+        <button onClick={() => this.postOne()}>post one post</button>
+      </div>
+    );
+  }
 }
 
 export default App;
