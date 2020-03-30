ca97895af3225f106d988ab4b753d5d6b3a823ee
Yuchen Lin <kloselyc@gmail.com>
2020-03-30 11:48:17 -0600

    finish alpha version of login/signup page, need to work on transition after successful login/signup

--- src/App.js
+++ src/App.js
@@ -1,112 +1,38 @@
 import React, { Component } from "react";
-import Axios from "axios";
-
-var backendUrl = "http://3.15.233.84:4000";
+import AuthPage from "./components/authPage";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      curUser: undefined,
-      curToken: undefined
-    };
-  }
   componentDidMount() {}
 
-  signupForm = e => {
-    e.preventDefault();
-    console.log(e.target.username.value);
-    let username = e.target.username.value;
-    let email = e.target.email.value;
-    let password = e.target.password.value;
-    let profile = {
-      username: username,
-      email: email,
-      password: password
-    };
+  // userprofile = () => {
+  //   console.log(this.state.curToken);
+  //   Axios.get(`${backendUrl}/userInfo`, {
+  //     headers: { "auth-token": window.sessionStorage.getItem("curToken") }
+  //   }).then(response => {
+  //     // this.setState({ curUser: response.data });
+  //     console.log(response.data);
+  //   });
+  // };
 
-    Axios.post(`${backendUrl}/auth/signup`, profile).then(response => {
-      console.log(response);
-    });
-  };
+  // postOne = () => {
+  //   let profile = {
+  //     content: "frontend test",
+  //     images: "images frontend test",
+  //     videos: "video frontend test",
+  //     likes: 2
+  //   };
+  //   Axios.post(`${backendUrl}/post/add`, profile, {
+  //     headers: { "auth-token": this.state.curToken }
+  //   }).then(response => {
+  //     // this.setState({ curUser: response.data });
+  //     console.log(response.data);
+  //   });
+  // };
 
-  loginForm = e => {
-    e.preventDefault();
-    let email = e.target.email.value;
-    let password = e.target.password.value;
-    let profile = {
-      email: email,
-      password: password
-    };
-
-    Axios.post(`${backendUrl}/auth/login`, profile).then(response => {
-      console.log(response);
-      this.setState({ curToken: response.data.accesstoken });
-    });
-  };
-
-  userprofile = () => {
-    console.log(this.state.curToken);
-    Axios.get(`${backendUrl}/userInfo`, {
-      headers: { "auth-token": this.state.curToken }
-    }).then(response => {
-      // this.setState({ curUser: response.data });
-      console.log(response.data);
-    });
-  };
-
-  postOne = () => {
-    let profile = {
-      content: "frontend test",
-      images: "images frontend test",
-      videos: "video frontend test",
-      likes: 2
-    };
-    Axios.post(`${backendUrl}/post/add`, profile, {
-      headers: { "auth-token": this.state.curToken }
-    }).then(response => {
-      // this.setState({ curUser: response.data });
-      console.log(response.data);
-    });
-  };
   render() {
     return (
-      <div>
-        <form
-          onSubmit={e => {
-            this.signupForm(e);
-          }}
-        >
-          <label>
-            UserName :<input type="text" name="username"></input>
-          </label>
-          <label>
-            Email :<input type="email" name="email"></input>
-          </label>
-          <label>
-            password :<input type="password" name="password"></input>
-          </label>
-
-          <input type="submit" value="Submit" />
-        </form>
-
-        <form
-          onSubmit={e => {
-            this.loginForm(e);
-          }}
-        >
-          <label>
-            Email :<input type="email" name="email"></input>
-          </label>
-          <label>
-            password :<input type="password" name="password"></input>
-          </label>
-
-          <input type="submit" value="Submit" />
-        </form>
-
-        <button onClick={() => this.userprofile()}>check user profile</button>
-        <button onClick={() => this.postOne()}>post one post</button>
+      <div className="App">
+        <AuthPage />
       </div>
     );
   }
---
+++ src/components/authPage.jsx
@@ -0,0 +1,159 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Logo from "../assets/logo.png";
+
+const BackendURL = "http://3.15.233.84:4000";
+export default class authPage extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  SignUp = e => {
+    e.preventDefault();
+    let username = e.target.username.value;
+    let email = e.target.email.value;
+    let password = e.target.password.value;
+    let Confirmpassword = e.target.Confirmpassword.value;
+
+    if (password !== Confirmpassword) {
+      window.confirm("Error : Password do not match");
+    } else {
+      let profile = {
+        username: username,
+        email: email,
+        password: password
+      };
+
+      axios.post(`${BackendURL}/auth/signup`, profile).then(response => {
+        console.log(response);
+      });
+
+      // route to another page after successful sign up
+    }
+  };
+
+  LogIn = e => {
+    e.preventDefault();
+    let email = e.target.email.value;
+    let password = e.target.password.value;
+    let profile = {
+      email: email,
+      password: password
+    };
+
+    axios.post(`${BackendURL}/auth/login`, profile).then(response => {
+      console.log(response);
+      // store secret token into browser session storage to stay login before browser is closed
+      window.sessionStorage.setItem("curToken", response.data.accesstoken);
+    });
+
+    //route to user profile page after successful login
+  };
+
+  render() {
+    return (
+      <div className="authPage">
+        <div className="authPage__logo">
+          {/* <img
+            className="authPage__logo--icon"
+            alt="this is the you connect logo"
+            src={Logo}
+          ></img> */}
+          <label className="authPage__logo--icon">YouConnect</label>
+        </div>
+        <div className="authPage__title">Welcome to YouConnect</div>
+        {/* Login Section */}
+        <form className="authPage__login" onSubmit={e => this.LogIn(e)}>
+          <label className="authPage__login--email">E-mail</label>
+          <input
+            className="authPage__login--emailInput"
+            name="email"
+            type="email"
+          />
+          <label className="authPage__login--password">Password</label>
+          <input
+            className="authPage__login--passwordInput"
+            name="password"
+            type="password"
+          />
+          <input
+            className="authPage__login--loginSubmit"
+            type="submit"
+            value="Log In"
+          />
+        </form>
+
+        {/* Signup Section */}
+        <form className="authPage__signup" onSubmit={e => this.SignUp(e)}>
+          <label className="authPage__signup--username">User Name</label>
+          <input
+            className="authPage__signup--usernameInput"
+            name="username"
+            type="text"
+          />
+          <label className="authPage__signup--email">E-mail</label>
+          <input
+            className="authPage__signup--emailInput"
+            name="email"
+            type="email"
+          />
+          <label className="authPage__signup--password">Password</label>
+          <input
+            className="authPage__signup--passwordInput"
+            name="password"
+            type="password"
+          />
+          <label className="authPage__signup--Confirmpassword">
+            Confirm Password
+          </label>
+          <input
+            className="authPage__signup--ConfirmpasswordInput"
+            name="Confirmpassword"
+            type="password"
+          />
+          <input
+            className="authPage__signup--signupSubmit"
+            type="submit"
+            value="Sign Up"
+          />
+        </form>
+
+        {/* Ask to register if not yet section */}
+        <div className="authPage__divideLine">
+          <hr />
+        </div>
+
+        <div className="authPage__askToRegister">
+          New to YouConnect?
+          <div
+            className="authPage__askToRegister--button"
+            onClick={() => {
+              console.log("Go to register section");
+              document.getElementsByClassName(
+                "authPage__login"
+              )[0].style.display = "none";
+              document.getElementsByClassName(
+                "authPage__divideLine"
+              )[0].style.display = "none";
+              document.getElementsByClassName(
+                "authPage__signup"
+              )[0].style.display = "flex";
+              document.getElementsByClassName(
+                "authPage__title"
+              )[0].style.height = "14%";
+              document.getElementsByClassName(
+                "authPage__title"
+              )[0].style.padding = "6%";
+              document.getElementsByClassName(
+                "authPage__askToRegister"
+              )[0].style.display = "none";
+            }}
+          >
+            Create Account
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
